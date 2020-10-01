2a371132fff64ba65237e32f19a4d4589ad1a8c9
Marcus10110 <markgarrison@saleae.com>
2020-09-30 20:42:14 -0700

    clang-format update of Management Data Input/Output (MDIO) Analyzer

--- src/MDIOAnalyzer.h
+++ src/MDIOAnalyzer.h
@@ -5,74 +5,87 @@
 #include "MDIOAnalyzerResults.h"
 #include "MDIOSimulationDataGenerator.h"
 
-enum MDIOPacketClauseType { MDIO_C22_PACKET, MDIO_C45_PACKET };
-enum MDIOPacketC45Type { MDIO_C45_PACKET_ADDR, MDIO_C45_PACKET_DATA };
-enum MDIOPacketOperation { MDIO_PACKET_READ, MDIO_PACKET_WRITE };
+enum MDIOPacketClauseType
+{
+    MDIO_C22_PACKET,
+    MDIO_C45_PACKET
+};
+enum MDIOPacketC45Type
+{
+    MDIO_C45_PACKET_ADDR,
+    MDIO_C45_PACKET_DATA
+};
+enum MDIOPacketOperation
+{
+    MDIO_PACKET_READ,
+    MDIO_PACKET_WRITE
+};
 
 // struct to take actions depending on the type of the current MDIO packet
-struct PacketType 
+struct PacketType
 {
-	MDIOPacketClauseType clause;
-	MDIOPacketC45Type c45Type;
-	MDIOPacketOperation operation;
+    MDIOPacketClauseType clause;
+    MDIOPacketC45Type c45Type;
+    MDIOPacketOperation operation;
 };
 
 class MDIOAnalyzerSettings;
 class MDIOAnalyzer : public Analyzer2
 {
-public:
-	MDIOAnalyzer();
-	virtual ~MDIOAnalyzer();
-	virtual void WorkerThread();
+  public:
+    MDIOAnalyzer();
+    virtual ~MDIOAnalyzer();
+    virtual void WorkerThread();
 
-	virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
-	virtual U32 GetMinimumSampleRateHz();
+    virtual U32 GenerateSimulationData( U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor** simulation_channels );
+    virtual U32 GetMinimumSampleRateHz();
 
-	virtual const char* GetAnalyzerName() const;
-	virtual bool NeedsRerun();
+    virtual const char* GetAnalyzerName() const;
+    virtual bool NeedsRerun();
     virtual void SetupResults();
-protected: 
-	void AdvanceToStartFrame();
-	
-	void ProcessStartFrame();
-	void ProcessOpcodeFrame();
-	void ProcessPhyAddrFrame();
-	void ProcessRegAddrDevTypeFrame();
-	void ProcessTAFrame();
-	void ProcessAddrDataFrame();
-	
-	void AdvanceToHighMDIO();
-	
-	void ProcessTAFrameInReadPacket();
-	void ProcessTAFrameInWritePacket();
 
-	void AddArrowMarkers();
-	void GetBit( BitState& bit_state, std::vector<U64> & arrows );
-	MDIOFrameType GetDevType(const U64 & value);
+  protected:
+    void AdvanceToStartFrame();
 
-protected: 
-	std::auto_ptr< MDIOAnalyzerSettings > mSettings;
-	std::auto_ptr< MDIOAnalyzerResults > mResults;
+    void ProcessStartFrame();
+    void ProcessOpcodeFrame();
+    void ProcessPhyAddrFrame();
+    void ProcessRegAddrDevTypeFrame();
+    void ProcessTAFrame();
+    void ProcessAddrDataFrame();
 
-	AnalyzerChannelData* mMdio;
-	AnalyzerChannelData* mMdc;
+    void AdvanceToHighMDIO();
 
-	MDIOSimulationDataGenerator mSimulationDataGenerator;
-	bool mSimulationInitialized;
+    void ProcessTAFrameInReadPacket();
+    void ProcessTAFrameInWritePacket();
 
-	// U8 mPacketInTransaction;
-	// U64 mTransactionID;
+    void AddArrowMarkers();
+    void GetBit( BitState& bit_state, std::vector<U64>& arrows );
+    MDIOFrameType GetDevType( const U64& value );
 
-	PacketType mCurrentPacket;
+  protected:
+    std::auto_ptr<MDIOAnalyzerSettings> mSettings;
+    std::auto_ptr<MDIOAnalyzerResults> mResults;
 
-	std::vector<U64> mMdcPosedgeArrows;
-	std::vector<U64> mMdcNegedgeArrows;
+    AnalyzerChannelData* mMdio;
+    AnalyzerChannelData* mMdc;
 
-	U32 mSampleRateHz;
+    MDIOSimulationDataGenerator mSimulationDataGenerator;
+    bool mSimulationInitialized;
+
+    // U8 mPacketInTransaction;
+    // U64 mTransactionID;
+
+    PacketType mCurrentPacket;
+
+    std::vector<U64> mMdcPosedgeArrows;
+    std::vector<U64> mMdcNegedgeArrows;
+
+    U32 mSampleRateHz;
 };
 
 extern "C" ANALYZER_EXPORT const char* __cdecl GetAnalyzerName();
-extern "C" ANALYZER_EXPORT Analyzer* __cdecl CreateAnalyzer( );
+extern "C" ANALYZER_EXPORT Analyzer* __cdecl CreateAnalyzer();
 extern "C" ANALYZER_EXPORT void __cdecl DestroyAnalyzer( Analyzer* analyzer );
 
-#endif //MDIO_ANALYZER_H
+#endif // MDIO_ANALYZER_H
--- src/MDIOAnalyzerSettings.cpp
+++ src/MDIOAnalyzerSettings.cpp
@@ -1,28 +1,26 @@
 #include "MDIOAnalyzerSettings.h"
 #include <AnalyzerHelpers.h>
 
-MDIOAnalyzerSettings::MDIOAnalyzerSettings()
-:	mMdioChannel( UNDEFINED_CHANNEL ),
-	mMdcChannel( UNDEFINED_CHANNEL )
+MDIOAnalyzerSettings::MDIOAnalyzerSettings() : mMdioChannel( UNDEFINED_CHANNEL ), mMdcChannel( UNDEFINED_CHANNEL )
 {
-	mMdioChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mMdioChannelInterface->SetTitleAndTooltip( "MDIO", "MDIO data bus" );
-	mMdioChannelInterface->SetChannel( mMdioChannel );
+    mMdioChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+    mMdioChannelInterface->SetTitleAndTooltip( "MDIO", "MDIO data bus" );
+    mMdioChannelInterface->SetChannel( mMdioChannel );
 
-	mMdcChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
-	mMdcChannelInterface->SetTitleAndTooltip( "MDC", "MDIO clock line " );
-	mMdcChannelInterface->SetChannel( mMdcChannel );
+    mMdcChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
+    mMdcChannelInterface->SetTitleAndTooltip( "MDC", "MDIO clock line " );
+    mMdcChannelInterface->SetChannel( mMdcChannel );
 
-	AddInterface( mMdioChannelInterface.get() );
-	AddInterface( mMdcChannelInterface.get() );
-	
-	AddExportOption( 0, "Export as text/csv file" );
-	AddExportExtension( 0, "text", "txt" );
-	AddExportExtension( 0, "csv", "csv" );
+    AddInterface( mMdioChannelInterface.get() );
+    AddInterface( mMdcChannelInterface.get() );
 
-	ClearChannels();
-	AddChannel( mMdioChannel, "MDIO", false );
-	AddChannel( mMdcChannel, "MDC", false );
+    AddExportOption( 0, "Export as text/csv file" );
+    AddExportExtension( 0, "text", "txt" );
+    AddExportExtension( 0, "csv", "csv" );
+
+    ClearChannels();
+    AddChannel( mMdioChannel, "MDIO", false );
+    AddChannel( mMdcChannel, "MDC", false );
 }
 
 MDIOAnalyzerSettings::~MDIOAnalyzerSettings()
@@ -31,49 +29,49 @@ MDIOAnalyzerSettings::~MDIOAnalyzerSettings()
 
 bool MDIOAnalyzerSettings::SetSettingsFromInterfaces()
 {
-	if( mMdioChannelInterface->GetChannel() == mMdcChannelInterface->GetChannel() )
-	{
-		SetErrorText( "MDIO and MDC can't be assigned to the same input." );
-		return false;
-	}
+    if( mMdioChannelInterface->GetChannel() == mMdcChannelInterface->GetChannel() )
+    {
+        SetErrorText( "MDIO and MDC can't be assigned to the same input." );
+        return false;
+    }
 
-	mMdioChannel = mMdioChannelInterface->GetChannel();
-	mMdcChannel = mMdcChannelInterface->GetChannel();
+    mMdioChannel = mMdioChannelInterface->GetChannel();
+    mMdcChannel = mMdcChannelInterface->GetChannel();
 
-	ClearChannels();
-	AddChannel( mMdioChannel, "MDIO", true );
-	AddChannel( mMdcChannel, "MDC", true );
+    ClearChannels();
+    AddChannel( mMdioChannel, "MDIO", true );
+    AddChannel( mMdcChannel, "MDC", true );
 
-	return true;
+    return true;
 }
 
 void MDIOAnalyzerSettings::UpdateInterfacesFromSettings()
 {
-	mMdioChannelInterface->SetChannel( mMdioChannel );
-	mMdcChannelInterface->SetChannel( mMdcChannel );
+    mMdioChannelInterface->SetChannel( mMdioChannel );
+    mMdcChannelInterface->SetChannel( mMdcChannel );
 }
 
 void MDIOAnalyzerSettings::LoadSettings( const char* settings )
 {
-	SimpleArchive text_archive;
-	text_archive.SetString( settings );
+    SimpleArchive text_archive;
+    text_archive.SetString( settings );
 
-	text_archive >> mMdioChannel;
-	text_archive >> mMdcChannel;
+    text_archive >> mMdioChannel;
+    text_archive >> mMdcChannel;
 
-	ClearChannels();
-	AddChannel( mMdioChannel, "MDIO", true );
-	AddChannel( mMdcChannel, "MDC", true );
+    ClearChannels();
+    AddChannel( mMdioChannel, "MDIO", true );
+    AddChannel( mMdcChannel, "MDC", true );
 
-	UpdateInterfacesFromSettings();
+    UpdateInterfacesFromSettings();
 }
 
 const char* MDIOAnalyzerSettings::SaveSettings()
 {
-	SimpleArchive text_archive;
+    SimpleArchive text_archive;
 
-	text_archive << mMdioChannel;
-	text_archive << mMdcChannel;
+    text_archive << mMdioChannel;
+    text_archive << mMdcChannel;
 
-	return SetReturnString( text_archive.GetString() );
+    return SetReturnString( text_archive.GetString() );
 }
